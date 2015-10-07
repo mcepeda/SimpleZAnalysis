77fc1fde8ee732fca1ff234a81cff84c9c99a542
mcepeda <maria.cepeda@cern.ch>
2015-10-07 14:13:35 +0200

    removed extra files

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/L1Trigger/SimpleZAnalysis/',1)[0])+'/cfipython/slc6_amd64_gcc491/L1Trigger/SimpleZAnalysis')
